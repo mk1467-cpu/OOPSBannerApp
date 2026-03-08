15d800808327d1f17fe2f2d7d54266f64687317a
mk1467-cpu <mk1467@srmist.edu.in>
2026-03-08 12:48:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# OOPS Banner App
+
+## Student Details
+•⁠  ⁠Name: MAGESHRAJ K
+•⁠  ⁠Register Number: RA2511003010227
+
+## Description
+This project prints an OOPS banner using Java.
+
+## Git Workflow
+•⁠  ⁠main branch
+•⁠  ⁠dev branch
+•⁠  ⁠feature-banner branch 
---
+++ program.java
@@ -0,0 +1,17 @@
+/**
+ * Printing OOPS
+ * @author Mageshraj K I
+ * @version 1.1
+ */
+
+public class program{
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*****", " ", "*****"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*    ", " ", "    *"));
+        System.out.println(String.join(" ", "*****", " ", "*****", " ", "*    ", " ", "*****"));                
+    }
+}
