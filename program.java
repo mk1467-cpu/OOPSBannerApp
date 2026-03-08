c1727709ee00e1418f2f91c7ccd321b916cd535e
mk1467-cpu <mk1467@srmist.edu.in>
2026-03-08 12:22:29 +0530

    Add program.java

---
+++ program.java
@@ -0,0 +1,15 @@
+public class program {
+    public static void main(String[] args) {
+		System.out.println("OOPS BANNER APP :\n");
+        System.out.println("   ***    " + "   ***    " + "******    " + "   ***** ");
+		System.out.println(" **   **  " + " **   **  " + "**    **  " + " **      ");
+		System.out.println("**     ** " + "**     ** " + "**     ** " + "**       ");
+		System.out.println("**     ** " + "**     ** " + "**    **  " + " **      ");
+		System.out.println("**     ** " + "**     ** " + "******    " + "   ***   ");
+		System.out.println("**     ** " + "**     ** " + "**        " + "      ** ");
+		System.out.println("**     ** " + "**     ** " + "**        " + "       **");
+		System.out.println(" **   **  " + " **   **  " + "**        " + "      **");
+		System.out.println("   ***    " + "   ***    " + "**        " + " *****");
+
+    }
+}
